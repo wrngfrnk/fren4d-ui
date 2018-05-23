5aa2ff915d9884b6865bba7bd08b425985a6c091
wrngfrnk <wrngfrnk@gmail.com>
2018-05-23 20:20:45 +0300

    Add API functionality

--- routes/frame.js
+++ routes/frame.js
@@ -3,9 +3,28 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/load/:anim', function(req, res, next) {
-    var file = JSON.parse(fs.readFileSync('animations/' + req.params.anim + '.json')); // Is synchronised file reading safe? Needs some testing.
+    function getFileWithExt() {
+        let ext = req.params.anim.indexOf('.json') > -1 ? '' : '.json';
+        return req.params.anim + ext;
+    } 
+
+    var file = JSON.parse(fs.readFileSync('animations/' + getFileWithExt())); // Is synchronised file reading safe? Needs some testing.
     res.setHeader('Content-Type', 'application/json');
     res.send(file);
+});
+
+router.get('/list', function(req, res, next) {
+    var list = {
+        files: []
+    };
+
+    fs.readdirSync('animations/').forEach(file => {
+        list.files.push(file);
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+    console.log(list)
+    res.send(JSON.stringify({...list, status: 'yep'}));
 });
 
 router.post('/save/:anim', function(req, res) {
